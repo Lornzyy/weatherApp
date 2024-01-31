d2c4dc79eeb5f77ff073eac380b59cf87c5cd5e3
Lornzyy <lornanish@gmail.com>
2024-02-01 00:25:46 +0300

    weather app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
+      <br />
+      <hr />
+      <p>
+        Made with ❤️ ©{year} by <a href="https://github.com/Lornzyy">Lorna</a>{" "}
+      </p>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [weatherCondition, setWeatherCondition] = useState({});
+
+  function showTemperature(response) {
+    // console.log(response);
+
+    if (response.data.message === "City not found") {
+      setWeatherCondition({
+        message: response.data.message,
+      });
+    } else {
+      setWeatherCondition({
+        city: response.data.city,
+        temperature: response.data.temperature.current,
+        country: response.data.country,
+        description: response.data.condition.description,
+        wind: response.data.wind.speed,
+        humidity: response.data.temperature.humidity,
+        icon: response.data.condition.icon_url,
+      });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let apiKey = "o63c6afa36060dtb755bc2adb841329a";
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(showTemperature);
+  }
+
+  function updateCity(e) {
+    e.preventDefault();
+    setCity(e.target.value);
+  }
+
+  if (!weatherCondition.message) {
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input
+            type="button"
+            value="Search"
+            className="btn"
+            onClick={handleSubmit}
+          />
+        </form>
+
+        <p>
+          Weather in {weatherCondition.city} {weatherCondition.country}
+        </p>
+
+        <p>
+          {" "}
+          <strong>Temperature </strong>
+          is {Math.round(weatherCondition.temperature)}°C
+        </p>
+        <p>
+          Description:<strong>{weatherCondition.description}</strong>
+        </p>
+        <p>
+          Humidity:<strong> {weatherCondition.humidity}%</strong>
+        </p>
+        <p>
+          Wind: <strong> {weatherCondition.wind}km/h</strong>
+        </p>
+        <img src={weatherCondition.icon} alt="Weather Icon" />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input
+            type="button"
+            value="Search"
+            className="btn"
+            onClick={handleSubmit}
+          />
+        </form>
+        <h3 className="text">
+          {weatherCondition.message}
+          <br />
+          <strong className="text"> Please enter a valid city</strong>
+        </h3>
+      </>
+    );
+  }
+}
